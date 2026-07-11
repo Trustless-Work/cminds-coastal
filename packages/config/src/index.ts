1d6875c19312d68c87c5ebcbc5f01db0a1a48550
Joel Vargas <vermudo.com@gmail.com>
2026-07-10 20:34:58 -0600

    fix: ci errors

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,19 +1,18 @@
 export { EnvConfig } from "./env-config";
 export { ClientEnv, clientEnv } from "./client-env";
 export { ServerEnv, serverEnv } from "./server-env";
-export {
-  NetworkConfig,
-  networkConfig,
-  type StellarNetworkId,
-  type WalletKitNetworkKey,
-  type NetworkAsset,
-  type TrustlineOption,
+export { NetworkConfig, networkConfig } from "./network-config";
+export type {
+  StellarNetworkId,
+  WalletKitNetworkKey,
+  NetworkAsset,
+  TrustlineOption,
 } from "./network-config";
 export {
   http,
   setAuthToken,
   setAuthTokenProvider,
   clearAuthToken,
-  type AuthTokenProvider,
 } from "./http";
+export type { AuthTokenProvider } from "./http";
 export { ApiError, handleApiError } from "./handle-errors";
